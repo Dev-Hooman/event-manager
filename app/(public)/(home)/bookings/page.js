fb13090aa314568e0705a03c76d5df20fcc2698f
AbdulRehman <abdulrehmansarfaraz90@gmail.com>
2025-02-10 12:57:47 +0500

    Implement session validation and redirection in MyBookingsPage and ProfilePage components

--- app/(public)/(home)/bookings/page.js
+++ app/(public)/(home)/bookings/page.js
@@ -1,10 +1,21 @@
-import BookingList from '@/components/bookings/BookingList'
-import React from 'react'
+import BookingList from "@/components/bookings/BookingList";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
+import React from "react";
 
-const MyBookingsPage = () => {
-  return (
-    <BookingList/>
-  )
-}
+const MyBookingsPage = async () => {
+  const session = await getServerSession(authOptions);
 
-export default MyBookingsPage
+  if (!session?.user) {
+    redirect("/login");
+  }
+
+  if (session?.user?.role === "user") {
+    return <BookingList />;
+  } else {
+    redirect("/");
+  }
+};
+
+export default MyBookingsPage;
--- app/(public)/(home)/profile/page.js
+++ app/(public)/(home)/profile/page.js
@@ -1,7 +1,16 @@
 import ProfileSettings from "@/components/SettingsPage/ProfileSettings";
 import React from "react";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
 
-const ProfilePage = () => {
+const ProfilePage = async () => {
+    const session = await getServerSession(authOptions);
+  
+    if (!session?.user) {
+      redirect("/login");
+    }
+  
   return (
     <div className="min-h-[calc(100vh)]">
       <ProfileSettings />
